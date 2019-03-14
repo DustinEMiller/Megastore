2964ea2037c12104ae01dcceb9489a1207fac641
DustinEMiller <dustinmiller032@gmail.com>
2019-03-13 19:58:55 -0500

    starting filter work

---
+++ Megastore/Models/FilterNavigation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Megastore.Models
+{
+    [NotMapped]
+    public class FilterNavigation {
+        public List<Category> categories = new List<Category>();
+        public List<Gender> genders = new List<Gender>();
+        public List<Site> site_ids = new List<Site>();
+        public List<Brand> brands = new List<Brand>();
+        public List<Size> sizes = new List<Size>();
+        public List<PriceRange> price_ranges = new List<PriceRange>();
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public void populateWithoutCategories() {
+            using (db) {
+                genders = db.Genders.ToList();
+                site_ids = db.Sites.ToList();
+                brands = db.Brands.ToList();
+                sizes = db.Sizes.ToList();
+            }
+        
+        }
+    }
+}
